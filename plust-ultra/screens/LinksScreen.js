7b21422021faa4b909dff39b2c1f70d66a58eaea
Adriel Fabella <adriel.fab@gmail.com>
2019-01-26 16:31:33 -0500

    Bob

--- plust-ultra/screens/LinksScreen.js
+++ plust-ultra/screens/LinksScreen.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import {GiftedChat} from 'react-native-gifted-chat';
 
+let id = 1;
 export default class LinksScreen extends React.Component {
-  state = {
-    messages: [],
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      typingText: null,
+    }
+    this.renderFooter = this.renderFooter.bind(this);
   }
 
   componentWillMount() {
@@ -25,10 +32,47 @@ export default class LinksScreen extends React.Component {
     })
   }
 
+  isTyping() {
+    this.setState({
+      typingText: 'Binky Is Typing'
+    });
+  }
+
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }))
+    this.isTyping();
+    id += 1;
+    setTimeout(() => {
+      const message = {
+        _id: id,
+        text: 'YOU SUCK',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      };
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, [message]),
+        typingText: null
+      }))
+    }, 1000);
+  }
+
+  renderFooter(props) {
+    if (this.state.typingText) {
+      return (
+        <View style={styles.footerContainer}>
+          <Text style={styles.footerText}>
+            {this.state.typingText}
+          </Text>
+        </View>
+      );
+    }
+    return null;
   }
 
   render() {
@@ -39,6 +83,7 @@ export default class LinksScreen extends React.Component {
         user={{
           _id: 1,
         }}
+        renderFooter={this.renderFooter}
       />
 
     );
@@ -50,5 +95,15 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  footerContainer: {
+    marginTop: 5,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  footerText: {
+    fontSize: 14,
+    color: '#aaa',
   },
 });
