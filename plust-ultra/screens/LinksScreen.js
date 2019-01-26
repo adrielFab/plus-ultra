f7482715ae1900459ae4d6fa400454d042c5e5db
Adriel Fabella <adriel.fab@gmail.com>
2019-01-26 16:14:16 -0500

    Initial

---
+++ plust-ultra/screens/LinksScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import {GiftedChat} from 'react-native-gifted-chat';
+
+export default class LinksScreen extends React.Component {
+  state = {
+    messages: [],
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
